f97c6b4b6946289d8b36d9774323762a3316fcbc
KantaKan <140788074+KantaKan@users.noreply.github.com>
2023-09-14 13:10:52 +0700

    cha

--- react project/src/component/Admin.jsx
+++ react project/src/component/Admin.jsx
@@ -8,7 +8,28 @@ function Admin() {
       </div><div className="button">
                 <button><Link to='/normal'>home</Link></button>
               <button><Link to='/owner'>owner</Link></button>
-          </div></>
+          </div>
+          <div className="table">
+          <table>
+  <tr>
+    <th>Name</th>
+    <th>Lastname</th>
+    <th>Position</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>  </td>
+    <td> </td>
+    <td></td>
+  </tr>
+</table>
+            </div></>
+          
 
 
   )
--- react project/src/component/Normal.jsx
+++ react project/src/component/Normal.jsx
@@ -16,6 +16,23 @@ function Normal() {
               <button><Link to='/admin'>Admin HOme Sector</Link></button>
           </div>
           <div className="table">
+          <table>
+  <tr>
+    <th>Name</th>
+    <th>Lastname</th>
+    <th>Position</th>
+  </tr>
+  <tr>
+    <td> </td>
+    <td> </td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>  </td>
+    <td> </td>
+    <td></td>
+  </tr>
+</table>
             </div></> 
 
 
